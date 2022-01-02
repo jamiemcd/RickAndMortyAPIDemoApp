f6ffda235554e34f5d3c68e33667b171bc67cde5
Jamie McDaniel <jamie@curiousfind.com>
2022-01-02 15:10:51 -0500

    Initial Commit

---
+++ RickAndMortyAPIDemo/RickAndMortyAPIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAPIDemoApp.swift
+//  RickAndMortyAPIDemo
+//
+//  Created by Jamie McDaniel on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAPIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
